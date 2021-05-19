b6c3d9b39b37b19f7f3ca7f6d4a44a58cf1d7b89
Lucas Goldman <lucasgoldman@optonline.net>
2021-05-19 13:47:57 -0400

    Fixed issue

--- app.py
+++ app.py
@@ -54,116 +54,6 @@ class Prediction(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     teams_df = pd.DataFrame()
 
-    def __init__(self, defensive_simple_rating_system, fumbles, interceptions, margin_of_victory, offensive_simple_rating_system,pass_net_yards_per_attempt,pass_touchdowns,pass_yards,penalties, percent_drives_with_points, percent_drives_with_turnovers, points_against,rank, rush_touchdowns, rush_yards, rush_yards_per_attempt, simple_rating_system, strength_of_schedule, turnovers,win_percentage, yards, yards_from_penalties, yards_per_play):
-        self.defensive_simple_rating_system = defensive_simple_rating_system
-        self.fumbles = fumbles
-        self.interceptions = interceptions
-        self.margin_of_victory = margin_of_victory
-        self.offensive_simple_rating_system = offensive_simple_rating_system
-        self.pass_net_yards_per_attempt = pass_net_yards_per_attempt
-        self.pass_touchdowns = pass_touchdowns
-        self.pass_yards = pass_yards
-        self.penalties = penalties
-        self.percent_drives_with_points = percent_drives_with_points
-        self.percent_drives_with_turnovers = percent_drives_with_turnovers
-        self.points_against = points_against
-        self.rank = rank
-        self.rush_touchdowns = rush_touchdowns
-        self.rush_yards = rush_yards
-        self.rush_yards_per_attempt = rush_yards_per_attempt
-        self.simple_rating_system = simple_rating_system
-        self.strength_of_schedule = strength_of_schedule
-        self.turnovers = turnovers
-        self.win_percentage = win_percentage
-        self.yards = yards
-        self.yards_from_penalties = yards_from_penalties
-        self.yards_per_play = yards_per_play
-
-    #@staticmethod
-    def probToMoneyLine (self,prob):
-        ml = 0
-        prob*=100
-        if prob >50:
-            ml = -(prob/(100 - prob)) * 100
-        elif prob < 50:
-            ml = (((100 - prob)/prob) * 100)
-        else:
-            ml = 100
-        ml = round(ml,2)
-        mlStr = str(ml)
-        if ml > 0:
-            mlStr = "+" + mlStr
-        return mlStr
-
-    def predict (self, team1, team2):
-        teams_df=self.getDF()
-        t1 = teams_df['percentile'].loc[team1]
-        t2 = teams_df['percentile'].loc[team2]
-        p = 1/(10**(-(t1 - t2))+1)
-        return self.probToMoneyLine(p)
-
-
-    def plot(self):
-        sns_plot = sns.kdeplot(self.teams_df['sum'])
-        sns_plot.savefig("output.png")
-
-    def predictGames(self):
-        games = getML()
-        for game in games:
-            team1 = game['teams'][0]
-            team2 = game['teams'][1]
-            # print(team1, ' vs', team2)
-            return 'Predicted Line for', team1,'is', predict(team1, team2)
-            # print('Actual Line for', team1,'is', game['odds'][0] )
-
-    def getDF(self):
-        teams_df = orig_df
-        for (columnName, columnData) in self.teams_df.iteritems(): 
-            if columnName != 'name':
-                teams_df[columnName] = stats.zscore(columnData)
-        teams_df['fumbles'] *= -1
-        teams_df['interceptions'] *= -1
-        teams_df['penalties'] *= -1
-        teams_df['percent_drives_with_turnovers'] *= -1
-        teams_df['points_against'] *= -1
-        teams_df['turnovers'] *= -1
-        teams_df['yards_from_penalties'] *= -1
-        rank = pd.Series()
-        rank['defensive_simple_rating_system'] = self.defensive_simple_rating_system
-        rank['fumbles'] =self.fumbles
-        rank['interceptions'] =self.interceptions
-        rank['margin_of_victory'] = self.margin_of_victory
-        rank['offensive_simple_rating_system'] = self.offensive_simple_rating_system
-        rank['pass_net_yards_per_attempt'] = self.pass_net_yards_per_attempt
-        rank['pass_touchdowns'] = self.pass_touchdowns
-        rank['pass_yards'] =self.pass_yards
-        rank['penalties'] =self.penalties
-        rank['percent_drives_with_points'] =self.percent_drives_with_points
-        rank['percent_drives_with_turnovers'] = self.percent_drives_with_turnovers
-        rank['points_against'] =self.points_against
-        rank['rank'] = self.rank
-        rank['rush_touchdowns'] = self.rush_touchdowns
-        rank['rush_yards'] = self.rush_yards
-        rank['rush_yards_per_attempt'] =self.rush_yards_per_attempt
-        rank['simple_rating_system'] = self.simple_rating_system
-        rank['strength_of_schedule'] = self.strength_of_schedule
-        rank['turnovers'] = self.turnovers
-        rank['win_percentage'] = self.win_percentage
-        rank['yards'] = self.yards
-        rank['yards_from_penalties'] = self.yards_from_penalties
-        rank['yards_per_play'] = self.yards_per_play
-        sum = rank.sum() 
-        rank/=sum
-        for (columnName, columnData) in rank.iteritems(): 
-            teams_df[columnName]*= columnData
-        teams_df['sum'] = 0.0
-        for i, row in teams_df.iterrows():
-            teams_df.at[i, 'sum'] = row['defensive_simple_rating_system':].sum()
-        teams_df.sort_values(by=['sum'], inplace=True, ascending=False)
-        teams_df['zscores'] = stats.zscore(teams_df['sum'])
-        teams_df['percentile'] =  1- stats.norm.sf(teams_df['zscores'])
-        return teams_df
-
     @staticmethod
     def getML():
         parser = argparse.ArgumentParser(description='Sample')
@@ -210,6 +100,85 @@ class Prediction(db.Model):
     def __repr__(self):
         return 'Prediction  Number: ' + str(self.id)
 
+def probToMoneyLine (prob):
+        ml = 0
+        prob*=100
+        if prob >50:
+            ml = -(prob/(100 - prob)) * 100
+        elif prob < 50:
+            ml = (((100 - prob)/prob) * 100)
+        else:
+            ml = 100
+        ml = round(ml,2)
+        mlStr = str(ml)
+        if ml > 0:
+            mlStr = "+" + mlStr
+        return mlStr
+
+def predict (pred, team1, team2):
+    teams_df= getDF(pred)
+    t1 = teams_df['percentile'].loc[team1]
+    t2 = teams_df['percentile'].loc[team2]
+    p = 1/(10**(-(t1 - t2))+1)
+    return probToMoneyLine(p)
+
+def predictGames(pred):
+        games = getML(pred)
+        for game in games:
+            team1 = game['teams'][0]
+            team2 = game['teams'][1]
+            # print(team1, ' vs', team2)
+            return 'Predicted Line for', team1,'is', predict(team1, team2)
+            # print('Actual Line for', team1,'is', game['odds'][0] )
+
+def getDF(pred):
+        teams_df = orig_df
+        for (columnName, columnData) in teams_df.iteritems(): 
+            if columnName != 'name':
+                teams_df[columnName] = stats.zscore(columnData)
+        teams_df['fumbles'] *= -1
+        teams_df['interceptions'] *= -1
+        teams_df['penalties'] *= -1
+        teams_df['percent_drives_with_turnovers'] *= -1
+        teams_df['points_against'] *= -1
+        teams_df['turnovers'] *= -1
+        teams_df['yards_from_penalties'] *= -1
+        rank = pd.Series()
+        rank['defensive_simple_rating_system'] = pred.defensive_simple_rating_system
+        rank['fumbles'] =pred.fumbles
+        rank['interceptions'] =pred.interceptions
+        rank['margin_of_victory'] = pred.margin_of_victory
+        rank['offensive_simple_rating_system'] = pred.offensive_simple_rating_system
+        rank['pass_net_yards_per_attempt'] = pred.pass_net_yards_per_attempt
+        rank['pass_touchdowns'] = pred.pass_touchdowns
+        rank['pass_yards'] =pred.pass_yards
+        rank['penalties'] =pred.penalties
+        rank['percent_drives_with_points'] =pred.percent_drives_with_points
+        rank['percent_drives_with_turnovers'] = pred.percent_drives_with_turnovers
+        rank['points_against'] =pred.points_against
+        rank['rank'] = pred.rank
+        rank['rush_touchdowns'] = pred.rush_touchdowns
+        rank['rush_yards'] = pred.rush_yards
+        rank['rush_yards_per_attempt'] =pred.rush_yards_per_attempt
+        rank['simple_rating_system'] = pred.simple_rating_system
+        rank['strength_of_schedule'] = pred.strength_of_schedule
+        rank['turnovers'] = pred.turnovers
+        rank['win_percentage'] = pred.win_percentage
+        rank['yards'] = pred.yards
+        rank['yards_from_penalties'] = pred.yards_from_penalties
+        rank['yards_per_play'] = pred.yards_per_play
+        sum = rank.sum() 
+        rank/=sum
+        for (columnName, columnData) in rank.iteritems(): 
+            teams_df[columnName]*= columnData
+        teams_df['sum'] = 0.0
+        for i, row in teams_df.iterrows():
+            teams_df.at[i, 'sum'] = row['defensive_simple_rating_system':].sum()
+        teams_df.sort_values(by=['sum'], inplace=True, ascending=False)
+        teams_df['zscores'] = stats.zscore(teams_df['sum'])
+        teams_df['percentile'] =  1- stats.norm.sf(teams_df['zscores'])
+        return teams_df
+
 @app.route('/') #base url
 def index():
     return render_template('index.html')
@@ -232,7 +201,7 @@ def predict(id):
     if request.method == 'POST':
         team1 = request.form['team1']
         team2 = request.form['team2']
-        ml = pred.predict(team1, team2)
+        ml = predict(pred, team1, team2)
         return render_template('predict.html',  pred=pred,team1=team1, team2=team2,ml=ml)
     else:
         return render_template('listteams.html', pred=pred)
@@ -240,7 +209,7 @@ def predict(id):
 @app.route('/prediction/view/<int:id>')
 def view(id):
     pred = Prediction.query.get_or_404(id)
-    view_df = pred.getDF()
+    view_df = getDF(pred)
     view_df = view_df.round(2)
     view_df = view_df[['sum', 'zscores', 'percentile']]
     img = io.BytesIO()
@@ -309,7 +278,7 @@ def new_prediction():
         yards = request.form['yards']
         yards_from_penalties = request.form['yards_from_penalties']
         yards_per_play = request.form['yards_per_play']
-        new_pred= Prediction(int(defensive_simple_rating_system), int(fumbles), int(interceptions), int(margin_of_victory), int(offensive_simple_rating_system),int(pass_net_yards_per_attempt),int(pass_touchdowns),int(pass_yards),int(penalties), int(percent_drives_with_points), int(percent_drives_with_turnovers), int(points_against),int(rank), int(rush_touchdowns), int(rush_yards), int(rush_yards_per_attempt), int(simple_rating_system), int(strength_of_schedule), int(turnovers),int(win_percentage), int(yards), int(yards_from_penalties), int(yards_per_play))
+        new_pred= Prediction(defensive_simple_rating_system=int(defensive_simple_rating_system), fumbles=int(fumbles), interceptions=int(interceptions), margin_of_victory=int(margin_of_victory), offensive_simple_rating_system=int(offensive_simple_rating_system),pass_net_yards_per_attempt=int(pass_net_yards_per_attempt),pass_touchdowns=int(pass_touchdowns),pass_yards=int(pass_yards),penalties=int(penalties), percent_drives_with_points=int(percent_drives_with_points), percent_drives_with_turnovers=int(percent_drives_with_turnovers), points_against=int(points_against),rank=int(rank), rush_touchdowns=int(rush_touchdowns), rush_yards=int(rush_yards), rush_yards_per_attempt=int(rush_yards_per_attempt), simple_rating_system=int(simple_rating_system), strength_of_schedule=int(strength_of_schedule), turnovers=int(turnovers),win_percentage=int(win_percentage), yards=int(yards), yards_from_penalties=int(yards_from_penalties), yards_per_play=int(yards_per_play))
         db.session.add(new_pred)
         db.session.commit()
         return redirect('/prediction')
